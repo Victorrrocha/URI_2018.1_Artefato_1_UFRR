5b3b5ee2cc4544c59791f9e657b93e8645310b60
Victor Barbosa Rocha <victor.barbosa.18@outlook.com>
2018-06-20 22:14:40 -0300

    Add files via upload

--- 1301 - produto do intervalo/main.c
+++ 1301 - produto do intervalo/main.c
@@ -1,24 +1,41 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <limits.h>
 
-//ÁRVORE DE SEGMENTO DA SOMA
-int buildsegTreeProd(int segTree[], int vet[], int low, int high, int pos){
+//ÁRVORE DE SEGMENTO DO PRODUTO
+int buildsegTree(int segTree[], int vet[], int low, int high, int pos){
 
     if(low == high){
         segTree[pos] = vet[low];
         return vet[low];
     } else {
         int mid = (low + high) / 2;
-        int somaEsq = buildsegTreeProd(segTree, vet, low, mid, 2 * pos + 1);
-        int somaDir = buildsegTreeProd(segTree, vet, mid + 1 , high, 2 * pos + 2);
-        segTree[pos] = somaEsq * somaDir;
+        int Esq = buildsegTree(segTree, vet, low, mid, 2 * pos);
+        int Dir = buildsegTree(segTree, vet, mid + 1 , high, 2 * pos + 1);
+        segTree[pos] = Esq * Dir;
         return segTree[pos];
     }
 }
 
+//TROCA E ATUALIZA UMA ARVORE DE SEGMENTO
+int troca(int segTree[], int low, int high, int indice, int pos, int valor){
+    if(indice == low && indice == high){
+        segTree[pos] = valor;
+        return segTree[pos];
+    }
+    if(indice > high || indice < low){
+        return 1;
+    }
+    int mid = (low + high) / 2;
+    int esq = troca(segTree, low, mid, indice, 2 * pos, valor);
+    int dir = troca(segTree, mid + 1, high, indice, 2 * pos + 1, valor);
+
+    return segTree[pos] = segTree[pos * 2] * segTree[pos * 2 + 1];
+}
+
 //BUSCA EM UMA ARVORE DE SEGMENTO
-int searchProd(int segTree[], int low, int high, int qlow, int qhigh, int pos){
+int search(int segTree[], int low, int high, int qlow, int qhigh, int pos){
 
     if(qlow <= low && qhigh >= high){
         return segTree[pos];
@@ -27,30 +44,63 @@ int searchProd(int segTree[], int low, int high, int qlow, int qhigh, int pos){
         return 1;
     }
     int mid = (low + high) / 2;
-    int somaEsq = searchProd(segTree, low, mid, qlow, qhigh, 2 * pos + 1);
-    int somaDir = searchProd(segTree, mid + 1, high, qlow, qhigh, 2 * pos + 2);
-    return somaEsq * somaDir;
+    int Esq = search(segTree, low, mid, qlow, qhigh, 2 * pos);
+    int Dir = search(segTree, mid + 1, high, qlow, qhigh, 2 * pos + 1);
+    return Esq * Dir;
 }
 
 int main() {
-    int* segTreeProd;
-    int* vet;
-    int z, tam_segTree;
-    int TAM, N, K, I, V, J;
+    int segTree[400005];
+    int vet[100005];
+    int m, tam_segTree;
+    int n, k, i, v, valor, result;
+    char op[1];
 
-    while(scanf("%d %d", &N, &K) != EOF)
-    {
-
+    for (m=1; m<100005; m++){
+        vet[m] = 1;
     }
 
+    while(scanf("%d %d",&n, &k) != EOF){
 
+        for(m = 1; m <= n; m++){
+            scanf("%d", &valor);
+            if(valor>0){
+                vet[m] = 1;
+            } else if(valor<0){
+                vet[m] = -1;
+            } else {
+                vet[m] = 0;
+            }
+        }
 
+        tam_segTree = 4*n-1;
 
-        tam_segTree = 2 * TAM - 1;
-        vet = (int*) malloc(TAM * sizeof(int));
-        segTreeprod = (int*) malloc(tam_segTree * sizeof(int));
+        buildsegTree(segTree, vet, 1, n, 1);
 
+        for(m = 0; m < k; m++){
+            scanf(" %c %d %d", &op, &i, &v);
+            if(op[0] == 'C'){
+                if(v>0){
+                    troca(segTree, 1, n, i, 1, 1);
+                } else if(v<0){
+                    troca(segTree, 1, n, i, 1, -1);
+                } else {
+                    troca(segTree, 1, n, i, 1, 0);
+                }
 
+            } else if(op[0] == 'P'){
+                result = search(segTree, 1, n, i, v, 1);
+                if(result>0){
+                    printf("+");
+                } else if(result<0){
+                    printf("-");
+                } else {
+                    printf("0");
+                }
+            }
+        }
+        printf("\n");
+    }
 
     return 0;
 }
